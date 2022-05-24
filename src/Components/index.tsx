af7d41bd17ac9da010425fb80a2197c71a1a2f86
gebec <gebec.m@gmail.com>
2022-05-24 23:07:19 +0200

    feature: created component index file

---
+++ src/Components/index.tsx
@@ -0,0 +1,6 @@
+export { Button } from './Button/Button';
+export { Footer } from './Footer/Footer';
+export { Game } from './Game/Game';
+export { Modal } from './Modal/Modal';
+export { Rules } from './Rules/Rules';
+export { ScoreBoard } from './ScoreBoard/ScoreBoard';
