5a3dee29b99684796f4aa50d706922f324cfefd8
gebec <gebec.m@gmail.com>
2022-06-11 23:08:31 +0200

    feature: displayed results

--- src/Components/Choice/Choice.tsx
+++ src/Components/Choice/Choice.tsx
@@ -3,9 +3,10 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 type TProps = {
-  onClickCallback: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  onClickCallback?: (event: React.MouseEvent<HTMLButtonElement>) => void;
   id: string;
   borderColor: string;
+  disabled?: boolean;
   children: React.ReactElement;
 };
 
@@ -13,19 +14,26 @@ export const Choice: React.FC<TProps> = ({
   onClickCallback,
   id,
   borderColor,
+  disabled = false,
   children,
 }) => {
   return (
-    <ChoiceComponent data-id={id} color={borderColor} onClick={onClickCallback}>
+    <ChoiceComponent
+      data-id={id}
+      color={borderColor}
+      onClick={onClickCallback}
+      disabled={disabled}
+    >
       {children}
     </ChoiceComponent>
   );
 };
 
 Choice.propTypes = {
-  onClickCallback: PropTypes.func.isRequired,
+  onClickCallback: PropTypes.func,
   id: PropTypes.string.isRequired,
   borderColor: PropTypes.string.isRequired,
+  disabled: PropTypes.bool,
   children: PropTypes.element.isRequired,
 };
 
@@ -34,8 +42,15 @@ const ChoiceComponent = styled.button`
   height: 10rem;
   border: 1rem solid ${(props) => props.color ?? '#00FF00'};
   border-radius: 50%;
+  cursor: pointer;
+  background-color: white;
 
-  > * {
+  &:disabled {
+    cursor: unset;
+    background-color: white;
+  }
+
+  & > * {
     width: 80%;
     height: 80%;
   }
--- src/Components/ResultsTable/ResultsTable.tsx
+++ src/Components/ResultsTable/ResultsTable.tsx
@@ -1,6 +1,15 @@
+import { ReactElement } from 'react';
+
 import PropTypes from 'prop-types';
 import { EOption } from '../../option.enum';
 import { EResult } from '../../result.enum';
+import { Choice } from '../Choice/Choice';
+
+import styled from 'styled-components';
+
+import Rock from '../../Assets/images/rock';
+import Paper from '../../Assets/images/paper';
+import Scissors from '../../Assets/images/scissors';
 
 type TProps = {
   userChoice: EOption | null;
@@ -13,12 +22,46 @@ export const ResultsTable: React.FC<TProps> = ({
   computerChoice,
   result,
 }) => {
+  const getChoice = (choice: EOption): ReactElement => {
+    switch (choice) {
+      case EOption.PAPER:
+        return <Paper />;
+      case EOption.ROCK:
+        return <Rock />;
+      case EOption.SCISSORS:
+        return <Scissors />;
+      default:
+        return <div></div>;
+    }
+  };
+
+  const getResult = (result: EResult | null): ReactElement => {
+    switch (result) {
+      case EResult.COMPUTER_WIN:
+        return <div>Computer win</div>;
+      case EResult.DRAW:
+        return <div>It&apos;s a draw</div>;
+      case EResult.USER_WIN:
+        return <div>You win</div>;
+      default:
+        return <div></div>;
+    }
+  };
+
   return (
-    <>
-      You picked: {userChoice}
-      Computer picked: {computerChoice}
-      Result: {result}
-    </>
+    <ResultsWrapper>
+      {getResult(result)}
+      {userChoice && (
+        <Choice id={EOption.ROCK} borderColor='#4055bf' disabled={true}>
+          {getChoice(userChoice)}
+        </Choice>
+      )}
+      {computerChoice && (
+        <Choice id={EOption.SCISSORS} borderColor='#ff002b' disabled={true}>
+          {getChoice(computerChoice)}
+        </Choice>
+      )}
+    </ResultsWrapper>
   );
 };
 
@@ -41,3 +84,10 @@ ResultsTable.defaultProps = {
   computerChoice: null,
   result: null,
 };
+
+const ResultsWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
